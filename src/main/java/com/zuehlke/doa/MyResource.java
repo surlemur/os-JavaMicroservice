3f00ea335ec1e2ddbadecfeaf80f0d928d56bfd0
Dominik Auf der Maur <dominik.aufdermaur@zuehlke.com>
2017-03-22 15:22:45 +0100

    log request; return non-constant result

--- src/main/java/com/zuehlke/doa/MyResource.java
+++ src/main/java/com/zuehlke/doa/MyResource.java
@@ -1,16 +1,26 @@
 package com.zuehlke.doa;
 
+import java.time.Instant;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("myResource")
 public class MyResource {
+    
+    private Logger log = LoggerFactory.getLogger(MyResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String get() {
-        return "My Resource!";
+    public String get(@Context HttpHeaders httpHeaders) {
+        log.debug("MyResource was queried with accept=" + httpHeaders.getAcceptableMediaTypes());
+        return "My Resource - it is now " + Instant.now();
     }
 }
